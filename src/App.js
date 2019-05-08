0b0181f2e3ed609a546c82c3ee949f0888c28c33
Koushik-G <koushikgelli@gmail.com>
2019-05-08 23:00:27 +0530

    components are build

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import { Row, Col } from "antd";
+import { Menu, Dropdown, Button, Icon, message } from "antd";
+import { PageHeader } from "antd";
+import Product from "./Product";
+import logo from "./logo.svg";
+import "./App.css";
+
+
 
 function App() {
+
+  const [currency, setCurrency] = useState("INR") 
+
+  function handleMenuClick({key}){
+
+  }
+
+  const menu = (
+    <Menu onClick={handleMenuClick}>
+      <Menu.Item key="INR"><Icon type="dollar" />INR</Menu.Item>
+      <Menu.Item key="USD"><Icon type="dollar" />USD</Menu.Item>
+    </Menu>
+  );
+  
+  function handleMenuClick(e) {
+    message.info('Click on menu item.');
+    console.log('click', e);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <PageHeader
+        title="Design Exercise"
+        extra={[
+          <Dropdown overlay={menu}>
+            <Button type="primary">
+              Currency<Icon type="down" />
+            </Button>
+          </Dropdown>
+        ]}
+      />
+      <Row>
+        <Col sm={24} md={8} lg={4}>
+          <Product />
+        </Col>
+        <Col sm={24} md={8} lg={4}>
+          <Product />
+        </Col>
+        <Col sm={24} md={8} lg={4}>
+          <Product />
+        </Col>
+        <Col sm={24} md={8} lg={4}>
+          <Product />
+        </Col>
+        <Col sm={24} md={8} lg={4}>
+          <Product />
+        </Col>
+        <Col sm={24} md={8} lg={4}>
+          <Product />
+        </Col>
+      </Row>
     </div>
   );
 }
+
+
 
 export default App;
